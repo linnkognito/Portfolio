73767d470e09c699ab94a13f2ce6a013203a9fad
Linn Johansen <linn.es.johansen@gmail.com>
2024-12-06 19:07:11 -0800

    Fixed map marker bug

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -30,18 +30,23 @@ const markers = [
 
 const geoData = feature(worldData, worldData.objects.land);
 
-// function MarkerText() {
-//   return (
-//     <div className='absolute m-6 flex flex-col gap-0 px-1 text-[0.7rem] font-semibold text-offwhite uppercase'>
-//       <p className='pl-4 leading-tight  rounded-t border-offwhite'></p>
-//       <span className='w-full h-[1em] border-t border-l border-offwhite'></span>
-//     </div>
-//   );
-// }
-
 function Map() {
   const [isHovered, setIsHovered] = useState(null);
-  const [markerIsHovered, setMarkerIsHovered] = useState(null);
+
+  function animateMarker(city) {
+    if (isHovered === city.location || (!isHovered && city.currentCity))
+      return true;
+
+    return false;
+  }
+
+  function showText(city) {
+    console.log(isHovered);
+    if (isHovered === city.location || (!isHovered && city.currentCity))
+      return true;
+
+    return false;
+  }
 
   return (
     <div className='realtive w-full h-auto bg-steel rounded shadow-subtle'>
@@ -63,18 +68,22 @@ function Map() {
               </Geographies>
 
               {/* Marker */}
-              {markers.map((m) => (
+              {markers.map((city) => (
                 <>
-                  <Marker key={m.label} coordinates={[m.pos.lng, m.pos.lat]}>
-                    <span>{m.label}</span>
+                  <Marker
+                    key={city.label}
+                    coordinates={[city.pos.lng, city.pos.lat]}
+                    className='p-10'
+                    onMouseEnter={() => setIsHovered(city.location)}
+                    onMouseLeave={() => setIsHovered(null)}
+                  >
+                    <span>{city.label}</span>
                     <svg
                       xmlns='http://www.w3.org/2000/svg'
                       className='overflow-visible cursor-pointer'
                       width='24'
                       height='24'
                       viewBox='0 0 24 24'
-                      onMouseEnter={() => setMarkerIsHovered(m)}
-                      onMouseLeave={() => setMarkerIsHovered(null)}
                     >
                       {/* Outer Ping Animation */}
                       <circle
@@ -84,20 +93,16 @@ function Map() {
                         fill='none'
                         stroke='#d9d6e0'
                         strokeWidth='2'
-                        className={`${
-                          (m.currentCity && !isHovered && !markerIsHovered) ||
-                          (isHovered && isHovered === m) ||
-                          (markerIsHovered && !m.currentCity)
+                        className={
+                          animateMarker(city)
                             ? 'animate-ping origin-center'
                             : ''
-                        }`}
+                        }
                       />
                       {/* Inner Static Circle */}
                       <circle cx='12' cy='12' r='4' fill='#d9d6e0' />
 
-                      {(isHovered === m ||
-                        (!isHovered && m.currentCity && !markerIsHovered) ||
-                        (markerIsHovered && !m.currentCity)) && (
+                      {showText(city) && (
                         <text
                           dy={17.6}
                           dx={25}
@@ -107,9 +112,11 @@ function Map() {
                             textTransform: 'uppercase',
                             fontWeight: 'bold',
                           }}
-                          className='animate-scaleFromCenter'
+                          className={
+                            showText(city) ? 'animate-scaleFromCenter' : ''
+                          }
                         >
-                          {m.value}
+                          {city.value}
                         </text>
                       )}
                     </svg>
@@ -122,14 +129,14 @@ function Map() {
       </Wrapper>
 
       <Wrapper cls='flex flex-col w-full items-center px-4 mb-4 gap-2'>
-        {markers.map((m) => (
+        {markers.map((city) => (
           <div
-            key={m.label}
+            key={city.label}
             className='w-full grid grid-cols-[1fr_auto_1fr] items-center gap-2 text-sm uppercase bg-midnight rounded'
           >
             {/* L a b e l */}
             <h5 className='justify-self-end font-semibold text-shadow-glow text-fluid break-words'>
-              [ {m.label} ]
+              [ {city.label} ]
             </h5>
             {/* S e p a r a t o r */}
             <span className='mx-2 text-center font-bold text-shadow-subtle text-ice'>
@@ -140,10 +147,10 @@ function Map() {
               className='text-left break-words text-fluid
             cursor-pointer hover:text-shadow-shine hover:animate-pulse
             '
-              onMouseEnter={() => setIsHovered(m)}
+              onMouseEnter={() => setIsHovered(city.location)}
               onMouseLeave={() => setIsHovered(null)}
             >
-              {m.value}
+              {city.value}
             </span>
           </div>
         ))}
